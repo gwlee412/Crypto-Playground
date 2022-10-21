1f70ca57b578ae078686b22ec39aa66382c7d7e7
Young Shim <hshim90@gmail.com>
2022-10-21 15:46:04 -0700

    added more containers

--- src/mainPageComponents/mainPage.js
+++ src/mainPageComponents/mainPage.js
@@ -7,17 +7,30 @@ import SearchBar from './searchBar';
 export default function MainPage() {
   return (
     <div className='App'>
-      <section id='section1'>
+      <section id='sec-1'>
         <h1 className='main-header'>One stop destination for all things music.</h1>
         <div className='flex-searchbar'>
           <SearchBar/>
         </div>
-        <a href="#section2">
+        <a href="#sec-2">
           <div className='scroll-button'></div>
         </a>
       </section>
-      <section id='section2'>
-        <h1>This is section 2.</h1>
+      <section id='sec-2'>
+        <div className='flex-info'>
+          <h1 className='info-header'>Get to know more about your favorite artists</h1>
+        </div>
+        <div id='pic-1'></div>
+        <div id='pic-2'></div>
+        <div className='flex-info'>
+          <h1 className='info-header'>Discover new artists</h1>
+        </div>
+      </section>
+
+      <section id='sec-3'>
+        <div className='tbd-body'>
+          <h1>That's about it for now</h1>
+        </div>
       </section>
     </div>
   )
